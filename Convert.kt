4150c3d7e578371c07a1fa6fae08cd7ce93305ec
Dennis Padilla <d_padilla.y@hotmail.com>
2023-05-29 14:08:24 -0400

    Added the user input /more conversions

--- Convert.kt
+++ Convert.kt
@@ -1,17 +1,98 @@
 fun main() {
-    var num: Int = 10;
-    var currentum: String = "g";
-    when (currentum) {
-    "km"-> println("The conversion to miles is ${num*0.62}")
-    "mi" -> println("The conversion to kilometers is ${num*1.61}")
-    "cm" -> println("The conversion to inches is ${num*0.39}")
-    "in" -> println("The conversion to centimeters is ${num*2.54}")
-    "kg" -> println("The conversion to pounds is ${num*2.2}")
-    "lb" -> println("The conversion to kilos is ${num*0.45}")
-    "g" -> println("The conversion to ounces is ${num*0.04}")
-    "oz" -> println("The conversion to grams is ${num*28.35}")
-    else -> {
-        print("No conversion")
+    print("Enter a numeric value: ")
+    val userInput: String? = readLine()
+    val value: Int? = userInput?.toInt()
+
+    print("Enter the current unit of measurement: ")
+    val unit: String? = readLine()
+
+    when (unit) {
+        "km" -> {
+            if (value != null) {
+                println("The conversion to miles is ${value * 0.62}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "mi" -> {
+            if (value != null) {
+                println("The conversion to kilometers is ${value * 1.61}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "cm" -> {
+            if (value != null) {
+                println("The conversion to inches is ${value * 0.39}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "in" -> {
+            if (value != null) {
+                println("The conversion to centimeters is ${value * 2.54}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "kg" -> {
+            if (value != null) {
+                println("The conversion to pounds is ${value * 2.2}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "lb" -> {
+            if (value != null) {
+                println("The conversion to kilos is ${value * 0.45}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "g" -> {
+            if (value != null) {
+                println("The conversion to ounces is ${value * 0.04}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "oz" -> {
+            if (value != null) {
+                println("The conversion to grams is ${value * 28.35}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "l" -> {
+            if (value != null) {
+                println("The conversion to cups ${value * 4.17}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "cup" -> {
+            if (value != null) {
+                println("The conversion to liters ${value * 0.24}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "C" -> {
+            if (value != null) {
+                println("The conversion to farenheit ${(value * 9/5)+32}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        "F" -> {
+            if (value != null) {
+                println("The conversion to celsius ${(value -32)*5/9}")
+            } else {
+                println("Invalid value")
+            }
+        }
+        else -> {
+            println("Invalid unit of measurement")
+        }
     }
-}
 }
